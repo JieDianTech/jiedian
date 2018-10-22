3e563202ef36f05bf07b3ee79216005a0b04f4dc
DESKTOP-V5VCPRP\maomao <372470125@qq.com>
2018-10-23 01:54:35 +0800

    首次Init，网页主站结构，提取出了header和footer

---
+++ msite.baidu.com/sdk/c.js
@@ -0,0 +1,18 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>您找的页面不见啦</title>
+    <link href="/Content/css/error.css" type="text/css" rel="stylesheet" />
+    <script src="/Content/js/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+    <div class="main">
+        <img src="/Content/img/404.png" alt="" />
+    </div>
+</body>
+</html>
